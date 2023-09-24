7dd91284b19d793807527fbdc37db1c0e3a0ce50
Botond Antalóczy <zane9907@gmail.com>
2023-09-24 21:30:03 +0200

    Ora02

---
+++ Ora_02/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace Ora_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Muvelet muvelet = new Muvelet(Osszeadas);
+            muvelet += Kulonbseg;
+            muvelet += Szorzas;
+            muvelet += Osztas;
+
+            //muvelet(10,5);
+            Szamok sz = new Szamok();
+            sz.Szamol(muvelet);
+            Console.WriteLine("--------------------");
+
+            sz.muveletEsemeny += muvelet;
+            //sz.muveletEsemeny += Osszeadas;
+            sz.Szamol();
+
+            muvelet = null;
+
+            Console.WriteLine("---------------------");
+
+
+            Action<int, int> action = delegate (int x, int y)
+            {
+                Console.WriteLine(x % y);
+            };
+
+            //action += Osszeadas;
+            action?.Invoke(10, 3);
+
+
+            Func<int, int, double> function = delegate (int x, int y)
+            {
+                return (x + y) / 2;
+            };
+
+            double? atlag = function?.Invoke(6, 5);
+
+            if (atlag != null)
+            {
+                Console.WriteLine(atlag);
+            }
+
+
+            Predicate<int> predicate = delegate (int x)
+            {
+                return x % 2 == 0;
+            };
+
+            bool parosE = predicate(2 + 7);
+
+            Console.WriteLine(parosE ? "A szám páros" : "A szám pártalan");
+
+
+            action += (x, y) =>
+            {
+                Console.WriteLine(x % y);
+            };
+
+
+            function += (x, y) =>
+            {
+                return (x + y) / 2d;
+            };
+
+
+            predicate += x =>
+            {
+                return x % 2 == 0;
+            };
+
+            Console.WriteLine("---------------");
+            action?.Invoke(10, 5);
+            Console.WriteLine(function?.Invoke(6, 5));
+            Console.WriteLine(predicate(3 + 9));
+
+        }
+
+        static void Osszeadas(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Kulonbseg(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Szorzas(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Osztas(int a, int b)
+        {
+            if (a != 0 || b != 0)
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
---
+++ Ora_02/Szamok.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ora_02
+{
+    public class Szamok
+    {
+        static Random rnd;
+
+        static Szamok()
+        {
+            rnd = new Random();
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public event Muvelet muveletEsemeny;
+
+        public Szamok()
+        {            
+            A = rnd.Next(1, 10);
+            B = rnd.Next(1, 10);
+        }
+
+        public void Szamol(Muvelet muvelet)
+        {
+            //if (muvelet != null)
+            //{
+            //    muvelet(A, B);
+            //}
+
+            muvelet?.Invoke(A, B);
+        }
+
+        public void Szamol()
+        {
+            muveletEsemeny?.Invoke(A, B);
+        }
+    }
+}
