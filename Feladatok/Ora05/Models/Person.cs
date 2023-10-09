02136eaf849d8ae63a8aa6d2e6ac26184406e65f
Antalóczy Botond <58741726+zane9907@users.noreply.github.com>
2023-10-09 16:48:03 +0200

    Add files via upload

---
+++ Feladatok/Ora05/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ora05.Models
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public Person()
+        {
+            Id = Guid.NewGuid().ToString();
+            Cars = new List<Car>();
+        }
+
+    }
+}
