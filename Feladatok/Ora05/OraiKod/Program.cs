cb52344196e3a4fdcf81e95780b7ad5fe60cba9f
Botond Antalóczy <zane9907@gmail.com>
2023-10-14 18:01:35 +0200

    Modified

---
+++ Feladatok/Ora05/OraiKod/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ora05
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarDbContext ctx = new CarDbContext();
+            var cars = ctx.Cars.ToList();
+            var people = ctx.People.ToList();
+
+            ;
+        }
+    }
+}
